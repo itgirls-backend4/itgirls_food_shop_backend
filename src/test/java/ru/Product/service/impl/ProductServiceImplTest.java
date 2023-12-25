a2c6ac6bba8aeedfaa63590e2a836d54ab972f4e
ksu-tei <2ksu.tei@gmail.com>
2023-12-25 21:14:26 +0300

    comment test

--- src/test/java/ru/Product/service/impl/ProductServiceImplTest.java
+++ src/test/java/ru/Product/service/impl/ProductServiceImplTest.java
@@ -31,26 +31,26 @@ public class ProductServiceImplTest {
     @InjectMocks
     ProductServiceImpl productService;
 
-    @Test
-    public void TestPurchaseItem() {
-        UUID productId = UUID.fromString("22c132ca-20c7-4f1e-b164-9e27143256f8");
-        Optional<Product> optionalProduct = productRepository.findById(productId);
-        Integer amountProduct = -1;
-        if (optionalProduct.isPresent()) {
-            Product existingProduct = optionalProduct.get();
-            amountProduct = existingProduct.getQuantity();
-        }
-        if (amountProduct == -1) {
-//            log.info("Данного продукта не существует");
-            System.out.println("Данного продукта не существует");
-        } else {
-            productService.purchaseItem(productId, amountProduct - 1);
-            productService.purchaseItem(productId, amountProduct + 1);
-            productService.purchaseItem(productId, 0);
-            productService.purchaseItem(productId, -1);
-            Product existingProduct = optionalProduct.get();
-            existingProduct.setQuantity(amountProduct);
-        }
-    }
+//    @Test
+//    public void TestPurchaseItem() {
+//        UUID productId = UUID.fromString("22c132ca-20c7-4f1e-b164-9e27143256f8");
+//        Optional<Product> optionalProduct = productRepository.findById(productId);
+//        Integer amountProduct = -1;
+//        if (optionalProduct.isPresent()) {
+//            Product existingProduct = optionalProduct.get();
+//            amountProduct = existingProduct.getQuantity();
+//        }
+//        if (amountProduct == -1) {
+////            log.info("Данного продукта не существует");
+//            System.out.println("Данного продукта не существует");
+//        } else {
+//            productService.purchaseItem(productId, amountProduct - 1);
+//            productService.purchaseItem(productId, amountProduct + 1);
+//            productService.purchaseItem(productId, 0);
+//            productService.purchaseItem(productId, -1);
+//            Product existingProduct = optionalProduct.get();
+//            existingProduct.setQuantity(amountProduct);
+//        }
+//    }
 
 }
